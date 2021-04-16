b047805bc88cd3747920ee1b6c829a0c235b8d36
satyadeepsingh <satyadeep.singh157@gmail.com>
2021-04-16 12:56:37 +0400

    My First commit

---
+++ Exercise Files/Ch2/conditionals_start.py
@@ -0,0 +1,20 @@
+#
+# Example file for working with conditional statements
+#
+
+
+def main():
+    x, y = 10, 100
+
+    # conditional flow uses if, elif, else
+    if(x < y):
+        print("x is less than y")
+    elif(x > y):
+        print("x is greater than y")
+    else:
+        print("x is equal to y")
+    # conditional statements let you use "a if C else b"
+
+
+if __name__ == "__main__":
+    main()
---
+++ README.md
@@ -0,0 +1,2 @@
+Hi I am @satyadeepsingh
+I am learning python in this repo
